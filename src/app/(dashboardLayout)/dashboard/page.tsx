271cd30c690fc9a68c9a7e17b1d8a80622245a52
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-12-28 21:58:14 +0600

    Fixed build issue

--- src/app/(dashboardLayout)/dashboard/page.tsx
+++ src/app/(dashboardLayout)/dashboard/page.tsx
@@ -1,13 +1,15 @@
 import DashboardOverview from '@/components/modules/dashboard/DashboardOverview';
 import { getStats } from '@/services/stats/stats.service';
+import {Suspense} from "react"
 
 export default async function UserDashboardOverviewPage() {
 
   const stats = await getStats()
-  console.log('stats', stats)
   return (
     <div>
+      <Suspense fallback={"Loading..."}>
       <DashboardOverview stats={stats} />
+      </Suspense>
     </div>
   );
 }
